4dec5e1b50a27b60ff2dee1a7ba91fbf7906c0a2
joseranieri <jose.ranieri22@gmail.com>
2019-10-02 17:02:35 -0300

    Iniciando configuracao controles

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,9 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: `Olá ${req.query.name}` });
-});
 
-routes.post('/devs', (req, res) => {
-    console.log(req.body);
-    return res.json({ ok: true });
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
