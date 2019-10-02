06f34af8991a206ab0806b60ed8c45f47deac783
joseranieri <jose.ranieri22@gmail.com>
2019-10-02 15:51:56 -0300

    Realizada documentação básica

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
+/**
+ * Código do servidor, que irá capturar as requisicoes
+ */
+
+//chamando o Express, framework para server web
 const express = require('express');
 
+//instanciando o express
 const server = express();
 
-
+//gerando uma resposta via Get retornando um Hello World
 server.get('/', (req, res) => {
     return res.send('Hello world');
 });
 
+//Startando o serviço escutando na porta 3333
 server.listen(3333);
