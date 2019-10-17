13143b5d1629a1fec0a56491807837fd85de101d
joseranieri <jose.ranieri22@gmail.com>
2019-10-17 17:03:50 -0300

    Finalizada base da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,20 @@
 //chamando o Express, framework para server web
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 //instanciando o express
 const server = express();
-server.use(express.json());
 //conectando com o banco
-mongoose.connect('mongodb+srv://tindev:tindev@cluster0-4mjmc.mongodb.net/tindev?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-4mjmc.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 //Startando o serviço escutando na porta 3333
 server.listen(3333);
